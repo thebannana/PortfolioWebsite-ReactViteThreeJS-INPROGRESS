ce5dac531fefc1e1289d30b9f8952ebed3a1340c
Denis Kundo <kundodenis@gmail.com>
2024-12-14 17:16:51 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "./Sections/Navbar.jsx";
+import Hero from "./Sections/Hero.jsx";
+
+function App() {
+    return (
+        <main className="max-w-7xl mx-auto">
+            <Navbar/>
+            <Hero/>
+        </main>
+    )
+}
+
+export default App
